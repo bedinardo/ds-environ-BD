4e4bfb0c16d104b9fac1c5d69437cc0c1948e818
bedinardo <bryce.dinardo@gmail.com>
2025-11-19 15:56:35 -0800

    lab 7

--- FIA.R
+++ FIA.R
@@ -6,14 +6,5 @@ library("rFIA")
 library("dplyr")
 library("tidyr")
 
-**Data aquistion**
-  Get all FIA data for Oregon by using rFIA
-```{r}
-options(timeout = 600) #Need to increase download time since file is so large
-#Grab the data
-OR <- getFIA(states = 'OR', dir = "C:/Users/dinardo/OneDrive - University Of Oregon/FIA", load = FALSE) #Install the data
-OR <- readFIA(dir = "C:/Users/dinardo/OneDrive - University Of Oregon/FIA",
-              states = 'OR',
-              nCores = 1,
-              inMemory = TRUE)
-```
+or_fia <- readFIA("C:/Users/Bryce/OneDrive - University Of Oregon/FIA", nCores = 4)
+or_fia <- clipFIA(or_fia)
